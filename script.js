0e4babc21b146935a4617579ec1868bcbf9ddcc9
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2025-03-12 23:38:39 +0500

    Finalized project

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 console.log("working...");
 
+// nav menu part
+let menu = document.querySelector("#menu");
+const openMenu = () => {
+    menu.style.right = "0";
+}
+const closeMenu = () => {
+    menu.style.right = "-200px";
+}
+
 // header part
 const showGame = () => {
     window.open("http://mathblitz-6.netlify.app", "_blank");
@@ -25,3 +34,20 @@ const openTab = (tabName) => {
     document.getElementById(tabName).classList.add("active_tab");
 }
 
+// feedback form data script
+const scriptURL = 'https://script.google.com/macros/s/AKfycby6-aWyrEd9ZgxWTi7LYQJe9C2Aj9mQlDUFUpcurZ-PPUyGODBdvwgVieEe9XfNClU2Bg/exec'
+const form = document.forms['submit-to-google-sheet']
+const message = document.getElementById("succ-msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        message.innerHTML = "Feedback sent successfully!";
+        setTimeout(function() {
+            message.innerHTML = "";
+        }, 5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
